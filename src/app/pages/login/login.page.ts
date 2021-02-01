88e0f3787ab7e8eac47fb47e442d46fc6dfe473b
Sara Cantero <cantero.mesar20@cadiz.salesianos.edu>
2021-02-01 12:41:41 +0100

    sara

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from '../../services/rest.service';
+import {AlertController} from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,18 +11,37 @@ import { RestService } from '../../services/rest.service';
 export class LoginPage implements OnInit {
 
   token: any;
+  email: any;
+  password:any;
+  
+  
 
-  constructor(public restService: RestService) {
-    this.hacerLogin();
+  constructor(public restService: RestService,private router: Router, private alertCtrl: AlertController) {
+    // this.hacerLogin();
    }
 
   ngOnInit() {
   }
 
-  hacerLogin(){
-    this.restService.login('raulreyes@gmail.com','123456').then(data => {
+ hacerLogin(){
+    this.restService.login(this.email, this.password).then(data => {
       console.log(data);
       this.token = data;
+      if(data){
+        console.log('Login correcto');
+        this.router.navigateByUrl('/tabs/tab1');;
+        
+      }
+      // else {
+      //   const alert = await this.alertCtrl.create({
+      //   header: 'ERROR',
+      //   subHeader: 'DATOS INCORRECTOS',
+      //   message: 'Los datos introducidos no corresponen a ningun usuario',
+      //   buttons: ['OK']
+      //   });
+      //   await alert.present();
+      //  }
+ 
     });
   }
 
