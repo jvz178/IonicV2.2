e507def5c63a42a7eb30ba8bda20ee4dbd44a186
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-02-04 12:27:44 +0100

    j

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -9,11 +9,18 @@ import { RestService } from '../../services/rest.service';
 export class Tab2Page {
 
   token: any;
+  filtro: any;
   offers: any;
+  df=0;
+  contador=0;
+  contador2=0;
+  clIDs = [];
+  clIDs2 = [];
 
   constructor(public restService: RestService,) {
     this.hacerLogin();
     this.obtenerOfertas();
+    this.obtenerCicloIds();
   }
 
   hacerLogin(){
@@ -25,15 +32,46 @@ export class Tab2Page {
 
   obtenerOfertas(){
     this.restService.getOffers()
-    .then(res => {
-      //if (res.sucess){
-        this.offers = res.data;
-        console.log(this.offers);
-      //}
+    .then((res: any) => {
+      this.offers=res.data;
+      console.log(this.offers);
+      if(this.df>0){
+        this.contador=0;
+        this.clIDs=[];
+        this.offers.forEach((id: { cicle_id: number; }) => {
+          console.log("FOREACH");
+          if(id.cicle_id==this.df){
+            console.log(id);
+            this.clIDs[this.contador]=id;
+            this.contador = this.contador+1;
+          }
+        });
+        this.offers = this.clIDs;
+      }
+        // this.filtro.filter( (offer: { cicle_id: number; }) => {
+        //   return offer.cicle_id == 5;
+        // })
     },
-    (error) =>{
-      console.error(error);
-    }
     );
+  }
+
+  onChange($dato: number){
+    this.df=$dato;
+    console.log("Filtro: "+this.df);
+
+    this.obtenerOfertas();
+  }
+
+  //Descartado pero podría ser útil...
+  obtenerCicloIds(){
+    this.restService.getOffers()
+    .then((res: any) => {
+
+      res.data.forEach((id2: any) => {
+        this.clIDs2[this.contador2]=id2.cicle_id;
+        this.contador2=this.contador2+1;
+      });
+      console.log(this.clIDs2);
+    })
   }
 }
